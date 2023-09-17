555c333a0c238abd9a76106b4d8ba08a54314888
DinaMo96 <93649274+DinaMostafa96@users.noreply.github.com>
2023-09-17 21:47:31 +0200

    Coding a Mandala Art

--- experiments/mandalaExp.js
+++ experiments/mandalaExp.js
@@ -1,54 +1,49 @@
 let x1, x2, y2, x3, y3, x4;
-let pedals;
+let pedals = 30;
+let angles;
 
 function setup() {
   createCanvas(500, 500);
 
   background(0);
   angleMode(DEGREES);
+  colorMode(HSB, 360, 100, 100, 100);
   translate(250, 250);
-  pedals = 30;
+  angles = 360 / pedals;
   x1 = random(185, 205);
   x4 = random(230, 245);
   x2 = random(190, 215);
-  y2 = random(5, 100);
+  let maxX2 = x2 * tan(angles) * 0.8;
+  y2 = random(5, maxX2);
   x3 = random(210, 230);
-  y3 = random(5, 100);
+  y3 = random(5, maxX2);
+  hue = random(256);
+  sat = random(70, 100);
+  brt = random(70, 100);
+  alph = random(70, 100);
+  fill(hue, sat, brt, alph);
+  for (let i = 0; i < pedals; i++) {
+    stroke(0, 0, 0);
+    beginShape();
+    curveVertex(x1, 0);
+    curveVertex(x1, 0);
+    curveVertex(x2, y2);
+    curveVertex(x3, y3);
+    curveVertex(x4, 0);
+    curveVertex(x4, 0);
+    endShape();
 
-  for (let i = 0; i < 360 / pedals; i++) beginShape();
-  curveVertex(x1, 0);
-  curveVertex(x1, 0);
-  curveVertex(x2, y2);
-  curveVertex(x3, y3);
-  curveVertex(x4, 0);
-  curveVertex(x4, 0);
-  endShape();
+    beginShape();
+    curveVertex(x1, 0);
+    curveVertex(x1, 0);
+    curveVertex(x2, -y2);
+    curveVertex(x3, -y3);
+    curveVertex(x4, 0);
+    curveVertex(x4, 0);
+    endShape();
+    // stroke(hue, sat, brt, alph);
+    // line(x1, 0, x4, 0);
 
-  beginShape();
-  curveVertex(x1, 0);
-  curveVertex(x1, 0);
-  curveVertex(x2, -y2);
-  curveVertex(x3, -y3);
-  curveVertex(x4, 0);
-  curveVertex(x4, 0);
-  endShape();
-
-  rotate(pedals);
-  beginShape();
-  curveVertex(x1, 0);
-  curveVertex(x1, 0);
-  curveVertex(x2, y2);
-  curveVertex(x3, y3);
-  curveVertex(x4, 0);
-  curveVertex(x4, 0);
-  endShape();
-
-  beginShape();
-  curveVertex(x1, 0);
-  curveVertex(x1, 0);
-  curveVertex(x2, -y2);
-  curveVertex(x3, -y3);
-  curveVertex(x4, 0);
-  curveVertex(x4, 0);
-  endShape();
+    rotate(angles);
+  }
 }
